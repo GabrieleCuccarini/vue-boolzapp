0fd064f1fcc436c4b7fbc9fd3a8025ebf60cb3b6
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-24 17:48:49 +0100

    Little updates

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@ const { createApp } = Vue;
 createApp({
     data () {
       return {
+        names: [
+          "Michele",
+          "Fabio", 
+          "Samuele",
+          "Alessandro B", 
+          "Alessandro L", 
+          "Claudia",
+          "Federico",
+          "Davide"
+        ],
         datiUtenti: [
             {
               name: 'Michele',
@@ -26,46 +36,46 @@ createApp({
               ],
             },
             {
-                name: 'Fabio',
-                avatar: '_2',
-                visible: true,
-                messages: [{
-                  date: '20/03/2020 16:30:00',
-                  message: 'Ciao come stai?',
-                  status: 'sent'
-                },
-                {
-                  date: '20/03/2020 16:30:55',
-                  message: 'Bene grazie! Stasera ci vediamo?',
-                  status: 'received'
-                },
-                {
-                  date: '20/03/2020 16:35:00',
-                  message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                  status: 'received'
-                }
-                ],
+              name: 'Fabio',
+              avatar: '_2',
+              visible: true,
+              messages: [{
+                date: '20/03/2020 16:30:00',
+                message: 'Ciao come stai?',
+                status: 'sent'
+              },
+              {
+                date: '20/03/2020 16:30:55',
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+              },
+              {
+                date: '20/03/2020 16:35:00',
+                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'received'
+              }
+              ],
             },
             {
-                name: 'Samuele',
-                avatar: '_3',
-                visible: true,
-                messages: [{
-                  date: '28/03/2020 10:10:40',
-                  message: 'La Marianna va in campagna',
-                  status: 'received'
-                },
-                {
-                  date: '28/03/2020 10:20:10',
-                  message: 'Sicuro di non aver sbagliato chat?',
-                  status: 'sent'
-                },
-                {
-                  date: '28/03/2020 16:15:22',
-                  message: 'Ah scusa!',
-                  status: 'received'
-                }
-                ],
+              name: 'Samuele',
+              avatar: '_3',
+              visible: true,
+              messages: [{
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
+              },
+              {
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+              },
+              {
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+              }
+              ],
             },
             {
               name: 'Luisa',
